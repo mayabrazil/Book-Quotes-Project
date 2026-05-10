9f3d5bbef106b6cca999a344c2eed0aa226ab688
Admin <hahmed20@uoguelph.ca>
2026-05-10 06:15:07 -0400

    new commits

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Server is WORKINGpy"
+    return render_template('postquote.html')
 
-if __name__ == "__main__":
+@app.route('/home')
+def home_feed():
+    return render_template('postquote.html')
+
+@app.route('/explore')
+def explore():
+    return render_template('feed.html')
+
+@app.route('/myshelf')
+def myshelf():
+    return render_template('myshelf.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
