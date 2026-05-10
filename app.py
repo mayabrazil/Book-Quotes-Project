0c1356c40259b2ba3aa94e80944ff7f1527f270c
Admin <hahmed20@uoguelph.ca>
2026-05-10 07:49:41 -0400

    404 done

--- app.py
+++ app.py
@@ -18,5 +18,9 @@ def explore():
 def myshelf():
     return render_template('myshelf.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
